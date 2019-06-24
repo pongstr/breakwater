b82cbc2775daa15eff139ff22931e4db5e34c7b9
Pongstr <twiz.tickler@gmail.com>
2019-06-24 22:39:57 +0300

    remove console.logs

--- src/app/components/image-info/image-info.component.ts
+++ src/app/components/image-info/image-info.component.ts
@@ -8,7 +8,5 @@ import { MAT_BOTTOM_SHEET_DATA } from '@angular/material';
 })
 export class ImageInfoComponent implements OnInit {
   constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: any) { }
-  ngOnInit() {
-    console.log(this.data)
-  }
+  ngOnInit() {}
 }
